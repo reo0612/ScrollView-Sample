32aefc9b7ab4aa74566eba1ca75f5210fdc95422
今西レオ <imanishi8033@gmail.com>
2020-11-26 14:33:07 +0900

    新規にScrollStackHorizontalフォルダを作成 その中に、ScrollStackHorizontalVCとScrollStackHorizontal.Storyboardを作成 そして、ScrollViewとStackViewを使用して3枚の画像が縦スクロールするViewを作成しました

---
+++ ScrollView-Sample/Controller/ScrollStackHorizontal/ScrollStackHorizontalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ScrollStackViewController.swift
+//  ScrollView-Sample
+//
+//  Created by 今西玲巳 on 2020/11/26.
+//
+
+import UIKit
+
+class ScrollStackHorizontalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+
+}
